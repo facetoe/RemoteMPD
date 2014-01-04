e802589d98d359f8f49753e0079c9815310d8206
facetoe <facetoe@ymail.com>
2014-01-04 18:43:10 +0800

    Beginning to flesh out the bluetooth listener. Only supports track change and playlist update for now.

--- RemoteMPD/src/com/facetoe/RemoteMPD/BluetoothMPDStatusMonitor.java
+++ RemoteMPD/src/com/facetoe/RemoteMPD/BluetoothMPDStatusMonitor.java
@@ -2,11 +2,15 @@ package com.facetoe.RemoteMPD;
 
 import android.util.Log;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.a0z.mpd.MPDStatus;
+import org.a0z.mpd.Music;
 import org.a0z.mpd.event.StatusChangeListener;
 import org.a0z.mpd.event.TrackPositionListener;
 
+import java.lang.reflect.Type;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by facetoe on 4/01/14.
@@ -16,17 +20,22 @@ public class BluetoothMPDStatusMonitor {
     private static final String TAG = RemoteMPDApplication.APP_TAG;
     private LinkedList<StatusChangeListener> statusChangedListeners = new LinkedList<StatusChangeListener>();
     private LinkedList<TrackPositionListener> trackPositionChangedListeners = new LinkedList<TrackPositionListener>();
-
+    Gson gson;
     public BluetoothMPDStatusMonitor() {
+        gson = new Gson();
     }
 
     public void handleMessage(MPDResponse response) {
         String jsonString = response.getObjectJSON();
+        MPDStatus status;
         switch (response.getResponseType()) {
-            case MPDResponse.PLAYER_UPDATE_CURRENTSONG:
+            case MPDResponse.EVENT_UPDATE_PLAYLIST:
+                extractSonglist(response);
+            break;
+            case MPDResponse.EVENT_TRACK:
+                status = gson.fromJson(jsonString, MPDStatus.class);
                 for (StatusChangeListener statusChangedListener : statusChangedListeners) {
-                    MPDStatus status = new Gson().fromJson(jsonString, MPDStatus.class);
-                    statusChangedListener.trackChanged(status, 1);
+                    statusChangedListener.trackChanged(status, -1);
                 }
                 break;
             default:
@@ -34,6 +43,13 @@ public class BluetoothMPDStatusMonitor {
                 break;
 
         }
+    }
+
+    private void extractSonglist(MPDResponse response) {
+        Type listType = new TypeToken<List<Music>>() {}.getType();
+        List<Music> songList = gson.fromJson(response.getObjectJSON(), listType);
+        RemoteMPDApplication.getInstance().setSongList(songList);
+        Log.i(TAG, "Added " + songList.size() + " songs");
     }
 
     /**
