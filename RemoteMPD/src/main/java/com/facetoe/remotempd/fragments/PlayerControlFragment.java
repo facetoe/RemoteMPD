de27c9cfc80223b98335a6866e9a74245cbc21de
facetoe <facetoe@ymail.com>
2014-03-07 17:32:13 +0800

    Wired up the PlayerControl fragment to follow track position etc

--- RemoteMPD/src/main/java/com/facetoe/remotempd/fragments/PlayerControlFragment.java
+++ RemoteMPD/src/main/java/com/facetoe/remotempd/fragments/PlayerControlFragment.java
@@ -1,13 +1,16 @@
 package com.facetoe.remotempd.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import com.facetoe.remotempd.R;
 import com.facetoe.remotempd.RMPDApplication;
 import com.facetoe.remotempd.helpers.MPDAsyncHelper;
@@ -15,15 +18,19 @@ import org.a0z.mpd.MPD;
 import org.a0z.mpd.MPDStatus;
 import org.a0z.mpd.Music;
 import org.a0z.mpd.event.StatusChangeListener;
+import org.a0z.mpd.event.TrackPositionListener;
 import org.a0z.mpd.exception.MPDServerException;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * RemoteMPD
- * <p/>
  * PlayerControlFragment handles controlling the MPD player.
  */
 public class PlayerControlFragment extends Fragment implements View.OnClickListener,
-        StatusChangeListener {
+        StatusChangeListener, TrackPositionListener {
 
     private static final String TAG = RMPDApplication.APP_PREFIX + "PlayerControlFragment";
     private final RMPDApplication app = RMPDApplication.getInstance();
@@ -36,11 +43,18 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     private ImageButton btnRepeat;
 
     private TextView txtSong;
-    private TextView txtAlbumArtist;
+    private TextView txtAlbum;
+    private TextView txtArtist;
+    private TextView totalTrackTime;
+    private TextView currentTrackTime;
+
     private SeekBar seekTrack;
     private SeekBar seekVolume;
 
     private LinearLayout nowPlayingLayout;
+    private long lastElapsedTime = 0;
+    private long lastSongTime = 0;
+    private Handler handler;
 
     private enum PLAYER_STATE {
         PLAYING,
@@ -51,6 +65,15 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
 
     private PLAYER_STATE playerState = PLAYER_STATE.UNKNOWN;
     private MPDStatus mpdStatus;
+
+    private Music currentSong;
+    Timer trackPositionTimer;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler = new Handler();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,13 +86,69 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
         btnRepeat = (ImageButton) rootView.findViewById(R.id.btnRepeat);
         btnRandom = (ImageButton) rootView.findViewById(R.id.btnRandom);
 
-        txtSong = (TextView)rootView.findViewById(R.id.txtSong);
-        txtAlbumArtist = (TextView)rootView.findViewById(R.id.txtAritstAlbum);
+        txtSong = (TextView) rootView.findViewById(R.id.txtSong);
+        txtAlbum = (TextView) rootView.findViewById(R.id.txtAlbum);
+        txtArtist = (TextView) rootView.findViewById(R.id.txtArtist);
+        totalTrackTime = (TextView) rootView.findViewById(R.id.totalTrackTime);
+        currentTrackTime = (TextView) rootView.findViewById(R.id.currentTrackTime);
 
-        seekTrack = (SeekBar)rootView.findViewById(R.id.seekTrackProgress);
-        seekVolume = (SeekBar)rootView.findViewById(R.id.seekVolume);
+        seekVolume = (SeekBar) rootView.findViewById(R.id.seekVolume);
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i(TAG, "progress changed: " + progress + " from user: " + fromUser);
+            }
 
-        nowPlayingLayout = (LinearLayout)rootView.findViewById(R.id.nowPlayingLayout);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.i(TAG, "progress stop: " + seekVolume.getProgress());
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Log.i(TAG, "Adjusting volume");
+                            mpd.setVolume(seekVolume.getProgress());
+                        } catch (MPDServerException e) {
+                            handleError(e);
+                        }
+                    }
+                }).start();
+            }
+        });
+
+        seekTrack = (SeekBar) rootView.findViewById(R.id.seekProgress);
+        seekTrack.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            mpd.seek((long) seekTrack.getProgress());
+                        } catch (MPDServerException e) {
+                            handleError(e);
+                        }
+                    }
+                }).start();
+            }
+        });
+
+        nowPlayingLayout = (LinearLayout) rootView.findViewById(R.id.nowPlayingLayout);
 
         btnPlay.setOnClickListener(this);
         btnNext.setOnClickListener(this);
@@ -92,6 +171,7 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     public void onStart() {
         super.onStart();
         asyncHelper.addStatusChangeListener(this);
+        asyncHelper.addTrackPositionListener(this);
         if (mpd.isConnected()) {
             initializePlayerBar();
         }
@@ -101,6 +181,7 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     public void onPause() {
         super.onPause();
         asyncHelper.removeStatusChangeListener(this);
+        asyncHelper.removeTrackPositionListener(this);
     }
 
     @Override
@@ -121,7 +202,9 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
                     setState(state);
                     setButtonPlayIcon();
                     updateRepeatRandomButtons(mpdStatus);
+                    setVolumeAndTrackPosition();
                     updateNowPlayingText(mpdStatus);
+                    maybeStartTrackPositionTimer();
 
                 } catch (MPDServerException e) {
                     handleError(e);
@@ -130,59 +213,56 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
         }).start();
     }
 
+    private void setVolumeAndTrackPosition() {
+        seekVolume.setProgress(mpdStatus.getVolume());
+        seekTrack.setMax((int) mpdStatus.getTotalTime());
+        seekTrack.setProgress((int) mpdStatus.getElapsedTime());
+    }
+
+
     private void updateRepeatRandomButtons(final MPDStatus status) {
-        Activity parentActivity = getActivity();
-        if (parentActivity != null) {
-            parentActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    setRepeatButtonIcon(status.isRepeat());
-                    setRandomButtonIcon(status.isRandom());
-                }
-            });
-        }
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                setRepeatButtonIcon(status.isRepeat());
+                setRandomButtonIcon(status.isRandom());
+            }
+        });
     }
 
     private void setButtonPlayIcon() {
-        Activity parentActivity = getActivity();
-        if (parentActivity != null) {
-            parentActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (playerState == PLAYER_STATE.PLAYING) {
-                        btnPlay.setImageResource(R.drawable.ic_media_pause);
-                    } else if (playerState == PLAYER_STATE.PAUSED || playerState == PLAYER_STATE.STOPPED) {
-                        btnPlay.setImageResource(R.drawable.ic_media_play);
-                    } else {
-                        Log.w(TAG, "Unknown state: " + playerState);
-                    }
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (playerState == PLAYER_STATE.PLAYING) {
+                    btnPlay.setImageResource(R.drawable.ic_media_pause);
+                } else if (playerState == PLAYER_STATE.PAUSED || playerState == PLAYER_STATE.STOPPED) {
+                    btnPlay.setImageResource(R.drawable.ic_media_play);
+                } else {
+                    Log.w(TAG, "Unknown state: " + playerState);
                 }
-            });
-        }
+            }
+        });
     }
 
-    private void updateNowPlayingText(MPDStatus status) {
+    private void updateNowPlayingText(final MPDStatus status) {
         final Music currentSong = getCurrentSong(status);
-        Activity parentActivity = getActivity();
-        if (parentActivity != null) {
-            parentActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-
-                    // If currentSong is null then the playlist is empty, so nothing is "now playing".
-                    if (currentSong == null) {
-                        Log.i(TAG, "Current song was null");
-                        nowPlayingLayout.setVisibility(View.GONE);
-                    } else {
-                        Log.i(TAG, "Updating text");
-                        nowPlayingLayout.setVisibility(View.VISIBLE);
-                        txtSong.setText(currentSong.getTitle());
-                        txtAlbumArtist.setText(currentSong.getArtist() + " - " + currentSong.getAlbum());
-                    }
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                // If currentSong is null then the playlist is empty.
+                if (currentSong == null) {
+                    nowPlayingLayout.setVisibility(View.GONE);
+                } else {
+                    nowPlayingLayout.setVisibility(View.VISIBLE);
+                    txtSong.setText(currentSong.getTitle());
+                    txtAlbum.setText(currentSong.getAlbum());
+                    txtArtist.setText(currentSong.getArtist());
+                    totalTrackTime.setText(timeToString(status.getTotalTime()));
+                    setVolumeAndTrackPosition();
                 }
-            });
-        }
-
+            }
+        });
     }
 
     private Music getCurrentSong(MPDStatus status) {
@@ -304,7 +384,7 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     @Override
     public void volumeChanged(MPDStatus mpdStatus, int oldVolume) {
         this.mpdStatus = mpdStatus;
-        Log.d(TAG, "Volume Changed");
+        seekVolume.setProgress(mpdStatus.getVolume());
     }
 
     @Override
@@ -317,9 +397,79 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     @Override
     public void trackChanged(MPDStatus mpdStatus, int oldTrack) {
         this.mpdStatus = mpdStatus;
-        Music currentSong = getCurrentSong(mpdStatus);
-        Log.d(TAG, "Track changed to: " + currentSong);
+        currentSong = getCurrentSong(mpdStatus);
         updateNowPlayingText(mpdStatus);
+        maybeStartTrackPositionTimer();
+    }
+
+    private void maybeStartTrackPositionTimer() {
+        setVolumeAndTrackPosition();
+        if (playerState == PLAYER_STATE.PLAYING) {
+            startTrackPositionTimer();
+        } else {
+            stopTrackPositionTimer();
+        }
+    }
+
+    @Override
+    public void trackPositionChanged(MPDStatus status) {
+        this.mpdStatus = status;
+        maybeStartTrackPositionTimer();
+    }
+
+    private void startTrackPositionTimer() {
+        stopTrackPositionTimer();
+        trackPositionTimer = new Timer();
+        TrackPositionTimerTask trackPositionTimerTask = new TrackPositionTimerTask(mpdStatus.getElapsedTime());
+        trackPositionTimer.scheduleAtFixedRate(trackPositionTimerTask, 0, 1000);
+    }
+
+    private void stopTrackPositionTimer() {
+        if (null != trackPositionTimer) {
+            trackPositionTimer.cancel();
+            trackPositionTimer = null;
+        }
+    }
+
+    private class TrackPositionTimerTask extends TimerTask {
+        Date date = new Date();
+        long start = 0;
+        long elapsed = 0;
+
+        public TrackPositionTimerTask(long start) {
+            this.start = start;
+        }
+
+        @Override
+        public void run() {
+            Date now = new Date();
+            elapsed = start + ((now.getTime() - date.getTime()) / 1000);
+            seekTrack.setProgress((int) elapsed);
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    currentTrackTime.setText(timeToString(elapsed));
+                }
+            });
+            lastElapsedTime = elapsed;
+        }
+    }
+
+    private static String timeToString(long seconds) {
+        if (seconds < 0) {
+            seconds = 0;
+        }
+
+        long hours = seconds / 3600;
+        seconds -= 3600 * hours;
+        long minutes = seconds / 60;
+        seconds -= minutes * 60;
+        if (hours == 0) {
+            return String.format("%02d:%02d", minutes, seconds);
+        } else {
+            return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        }
     }
 
     @Override
@@ -327,6 +477,7 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
         this.mpdStatus = mpdStatus;
         setState(mpdStatus.getState());
         setButtonPlayIcon();
+        maybeStartTrackPositionTimer();
     }
 
     @Override
@@ -336,7 +487,7 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     }
 
     private void setRepeatButtonIcon(boolean repeating) {
-        if(repeating) {
+        if (repeating) {
             btnRepeat.setImageResource(R.drawable.ic_media_repeat_on);
         } else {
             btnRepeat.setImageResource(R.drawable.ic_media_repeat);
@@ -350,7 +501,7 @@ public class PlayerControlFragment extends Fragment implements View.OnClickListe
     }
 
     private void setRandomButtonIcon(boolean random) {
-        if(random) {
+        if (random) {
             btnRandom.setImageResource(R.drawable.ic_media_shuffle_on);
         } else {
             btnRandom.setImageResource(R.drawable.ic_media_shuffle);
